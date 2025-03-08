67cda841ecb24797afb8a8a6e7ad8a550101e833
caesar <454451758@qq.com>
2025-03-08 14:20:45 +0800

    chore: pm2

---
+++ pm2.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  apps: [
+    {
+      name: "uni",
+      exec_mode: "cluster",
+      instances: "max",
+      args: "start",
+      script: "./node_modules/next/dist/bin/next",
+      watch: false,
+      autorestart: true,
+      combine_logs: true,
+      env: {
+        NODE_ENV: "production",
+        PORT: 3000,
+      },
+      exp_backoff_restart_delay: 100,
+      ignore_watch: ["node_modules"],
+      log_date_format: "YYYY-MM-DD HH:mm Z",
+      pid_file: "/tmp/.pm2/pid/cmc.pid",
+      error_file: "/tmp/.pm2/logs/cmc_err.log",
+    },
+    {
+      name: "uni-auth",
+      exec_mode: "cluster",
+      instances: 1,
+      args: "start",
+      script: "./dist/server.js",
+      watch: false,
+      autorestart: true,
+      combine_logs: true,
+      env: {
+        NODE_ENV: "production",
+        PORT: 3000,
+      },
+      exp_backoff_restart_delay: 100,
+      log_date_format: "YYYY-MM-DD HH:mm Z",
+      pid_file: "/tmp/.pm2/pid/cmc.pid",
+      error_file: "/tmp/.pm2/logs/cmc_auth_err.log",
+    },
+  ],
+};
